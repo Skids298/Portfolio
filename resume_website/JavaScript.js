c3e96ec200955644d50f53db19b719a0b6e1bbb5
Skids298 <npdevries@cru.umhb.edu>
2024-09-05 16:21:51 -0500

    9/5/2024 4:21

--- resume_website/JavaScript.js
+++ resume_website/JavaScript.js
@@ -31,20 +31,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function centerImage(img) {
     var overlay = document.getElementById("overlay");
+    var scrollableBlock = document.getElementById("scrollable-block");
 
     if (img.classList.contains('sticky')) {
         // Remove the sticky class to return to the original position
         img.classList.remove('sticky');
+        scrollableBlock.style.display = "none"; // Hide the scrollable block
         overlay.style.display = "none"; // Hide the overlay
     } else {
         // Remove any previously sticky images
         var existingStickyImage = document.querySelector('.sticky');
         if (existingStickyImage) {
             existingStickyImage.classList.remove('sticky');
+            document.getElementById("scrollable-block").style.display = "none"; // Hide scrollable block if another image was sticky
         }
 
         // Add sticky class to the clicked image
         img.classList.add('sticky');
+        scrollableBlock.style.display = "block"; // Show the scrollable block
         overlay.style.display = "block"; // Show the overlay
     }
 }
